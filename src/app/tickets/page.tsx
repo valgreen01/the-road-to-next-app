547e952e3ec679fdd6cc17e44814f636e942a894
Aldo Valverde <alvane.01@gmail.com>
2025-05-30 21:43:08 -0600

    TypeScript

--- src/app/tickets/page.tsx
+++ src/app/tickets/page.tsx
@@ -2,11 +2,18 @@ import Link from 'next/link';
 import { initialTickets } from '@/data';
 import { ticketPath } from '@/paths';
 
+const TICKETS_ICONS = {
+	OPEN: '🟢',
+	DONE: '✅',
+	IN_PROGRESS: '🔵',
+}
+
 const TicketsPage = () => {
 	return (
 		<div>
 			{initialTickets.map((ticket) => (
 				<div key={ticket.id} className="mb-6">
+					<div>{TICKETS_ICONS[ticket.status]}</div>
 					<h2 className="text-lg">{ticket.title}</h2>
 					<Link href={ticketPath(ticket.id)} className="text-sm underline">View</Link>
 				</div>
--- src/data.ts
+++ src/data.ts
@@ -3,12 +3,24 @@ export const initialTickets = [
 		id: 1,
 		title: 'Ticket 1',
 		description: 'Description for ticket 1',
-		status: 'DONE',
+		status: 'DONE' as const,
 	},
 	{
 		id: 2,
 		title: 'Ticket 2',
 		description: 'Description for ticket 2',
-		status: 'OPEN',
+		status: 'OPEN' as const,
+	},
+	{
+		id: 3,
+		title: 'Ticket 3',
+		description: 'Description for ticket 3',
+		status: 'OPEN' as const,
+	},
+	{
+		id: 4,
+		title: 'Ticket 4',
+		description: 'Description for ticket 4',
+		status: 'IN_PROGRESS' as const,
 	},
 ];
